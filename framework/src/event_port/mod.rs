f4bd9c570e573802da2474cfe78688b228b82625
Accumulator <19354678+accmltr@users.noreply.github.com>
2025-07-23 12:58:34 +1000

    save

--- framework/src/event_port/mod.rs
+++ framework/src/event_port/mod.rs
@@ -20,3 +20,38 @@ pub trait ClientPort<SIFromServer, ClientSIToServer> {
     /// Send messages at the end of each step.
     fn send(&mut self);
 }
+
+/// 1. Creates an endpoint for either client or server.
+/// 2. Accepts QUIC connections and does auth.
+/// 3. If auth success, creates port connection.
+/// 4. Collects messages from senders to simulation, between read calls.
+/// Note: Locally "sent" messages are tagged as such.
+pub trait Port<Id, SI, IO>
+where
+    Id: Eq,
+{
+    /// Return messages received since last `send()` call by connection ID.
+    fn read(&self) -> Vec<ReceivedMessages<Id, SI>>;
+
+    fn send(&mut self, recipient_id: Id, message: IO) {}
+}
+
+pub struct ReceivedMessages<Id, Message> {
+    id: Id,
+    received_locally: bool,
+    messages: Vec<Message>,
+}
+
+impl<Id, Message> ReceivedMessages<Id, Message> {
+    pub fn id(&self) -> &Id {
+        &self.id
+    }
+
+    pub fn received_locally(&self) -> bool {
+        self.received_locally
+    }
+
+    pub fn messages(&self) -> &Vec<Message> {
+        &self.messages
+    }
+}

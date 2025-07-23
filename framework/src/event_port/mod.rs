b2d8317bcb1694f262db27cb5fde837be0d9ea46
Accumulator <19354678+accmltr@users.noreply.github.com>
2025-07-23 15:24:52 +1000

    save

--- framework/src/event_port/mod.rs
+++ framework/src/event_port/mod.rs
@@ -1,48 +1,45 @@
-pub trait ServerPort<SI, SO> {
-    /// Return client inputs to stepper.
-    fn stepper_read(&mut self) -> SI;
-
-    /// Broadcast the step input used on server for all clients
-    /// to replicate.
-    fn sim_out_to_all(&mut self, stepper_output: SO);
-
-    /// Send messages at the end of each step.
-    fn send(&mut self);
-}
-
-pub trait ClientPort<SIFromServer, ClientSIToServer> {
-    /// Read stepper input received from server.
-    fn stepper_read(&mut self) -> SIFromServer;
-
-    /// Message from client to server.
-    fn sim_in_to_server(&mut self, message: ClientSIToServer);
-
-    /// Send messages at the end of each step.
-    fn send(&mut self);
-}
-
 /// 1. Creates an endpoint for either client or server.
 /// 2. Accepts QUIC connections and does auth.
 /// 3. If auth success, creates port connection.
-/// 4. Collects messages from senders to simulation, between read calls.
+/// 4. Collects and coalesses messages from senders to simulation, between
+///    `send()` calls.
 /// Note: Locally "sent" messages are tagged as such.
-pub trait Port<Id, SI, IO>
-where
-    Id: Eq,
-{
-    /// Return messages received since last `send()` call by connection ID.
-    fn read(&self) -> Vec<ReceivedMessages<Id, SI>>;
+pub trait Port<SI> {
+    /// Return client inputs to stepper.
+    fn stepper_read(&mut self) -> SI;
 
-    fn send(&mut self, recipient_id: Id, message: IO) {}
+    /// Send messages at the end of each step.
+    fn send(&mut self);
 }
 
-pub struct ReceivedMessages<Id, Message> {
+pub trait ClientPort<SIFromServer, ClientSIToServer>: Port<SIFromServer> {
+    /// Message from client to server.
+    fn sim_in_to_server(&mut self, message: ClientSIToServer);
+}
+
+pub trait ServerPort<SI, SO>: Port<SI> {
+    /// Broadcast the step input used on server for all clients
+    /// to replicate.
+    fn sim_out_to_all(&mut self, stepper_output: SO);
+}
+
+pub struct TotalStepInput<Id, Event> {
+    all_coalessed: Vec<CoalessedEvents<Id, Event>>,
+}
+
+impl<Id, Event> TotalStepInput<Id, Event> {
+    pub fn all_coalessed(&self) -> &Vec<CoalessedEvents<Id, Event>> {
+        &self.all_coalessed
+    }
+}
+
+pub struct CoalessedEvents<Id, Event> {
     id: Id,
     received_locally: bool,
-    messages: Vec<Message>,
+    events: Vec<Event>,
 }
 
-impl<Id, Message> ReceivedMessages<Id, Message> {
+impl<Id, Message> CoalessedEvents<Id, Message> {
     pub fn id(&self) -> &Id {
         &self.id
     }
@@ -51,7 +48,7 @@ impl<Id, Message> ReceivedMessages<Id, Message> {
         self.received_locally
     }
 
-    pub fn messages(&self) -> &Vec<Message> {
-        &self.messages
+    pub fn events(&self) -> &Vec<Message> {
+        &self.events
     }
 }

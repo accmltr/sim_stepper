e6e4f5dca911253f497ed7fe5bfba0dbdadf08ee
Accumulator <19354678+accmltr@users.noreply.github.com>
2025-07-27 02:48:38 +1000

    save

--- framework/src/lib.rs
+++ framework/src/lib.rs
@@ -1,3 +1,13 @@
+//! Create a game that is also a deterministic, event-based simulation and get
+//! reliable multiplayer functionality, implemented for you.
+//!
+//! Purpose:
+//! - Replicates determistic simulations accross QUIC connections.
+//! - Accepts runtime/local connections for local event generation and custom
+//!   connection solutions to be handled by runtime.
+//! - Provides various simulation managers which handle message passing between
+//!   local simulation(s) and the port.
+//! - Provides port implementation for networking functionality via QUIC.
 pub mod event_port;
 pub mod simulation;
 pub mod stepper;

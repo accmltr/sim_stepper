b896001c53100d6a8866a5a602f8a97bb11490c6
Accumulator <19354678+accmltr@users.noreply.github.com>
2025-07-29 03:07:41 +1000

    save

--- premade_ports/src/quinn/server/mod.rs
+++ premade_ports/src/quinn/server/mod.rs
@@ -1,9 +1,30 @@
+use std::thread;
+
 use framework::{
     event::Event,
     event_port::{Port, ServerPort},
 };
 
 pub struct QuinnServerPort {}
+
+impl QuinnServerPort {
+    pub fn new() {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        thread::spawn(move || {
+            let runtime = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .expect("Failed to build tokio runtime.");
+
+            runtime.block_on(async {
+                run_networking().await;
+            });
+        });
+    }
+}
+
+async fn run_networking() {}
 
 impl<E, EventSourceID> Port<E, EventSourceID> for QuinnServerPort
 where
